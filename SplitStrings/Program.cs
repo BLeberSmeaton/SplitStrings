8d3161da186352d1358f6928422dbb2dc0ce04fb
BLeberSmeaton <bella.lebersmeaton@myob.com>
2022-10-18 13:05:30 +1100

    Solved

--- SplitStrings/Program.cs
+++ SplitStrings/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿var str = "abcdef";
+var underscore = "_";
+var evenStr = "";
+var result = new List<string>();
+
+if (str.Length % 2 != 0)
+{
+	evenStr = str + underscore;
+}
+else
+{
+	evenStr = str;
+}
+
+for (int i = 0; i < evenStr.Length; i+=2)
+{
+	var pair = evenStr.Substring(i,2);
+	result.Add(pair);
+}
+
+var resultArr = result.ToArray();
+
+foreach (var i in resultArr)
+{
+	Console.WriteLine(i);
+}
+
